6b4e2562f780c63120aef89323588018d181cbce
Kauê de Magalhães <61990018+kkkaue@users.noreply.github.com>
2022-01-25 09:47:39 -0300

    Update Inverter_lista.py

--- Inverter_lista.py
+++ Inverter_lista.py
@@ -1,18 +1,18 @@
 
 def inverter_lista(lista):
-	tamanho = len(lista)-1
+	tamanho = len(lista)
 	limite = tamanho//2
 	for i in range(limite):
 		aux = lista[i]
-		lista[i] = lista[tamanho - i]
+		lista[i] = lista[tamanho - i - 1]
 		lista[tamanho -  i] = aux
 		print(lista)
 
 def inverter_lista2(lista):
 	nova_lista = []
-	tamanho = len(lista)-1
-	for i in range(tamanho+1):
-		nova_lista.append(lista[tamanho-i])
+	tamanho = len(lista)
+	for i in range(tamanho):
+		nova_lista.append(lista[tamanho - i - 1])
 	print(nova_lista)
 
 inverter_lista(["a", "b", "c"])
